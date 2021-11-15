40e6b99e5fa9a8ceeadf9fb3eeaf0c27a35f1a08
danielleleigh <danielle.leigh90@gmail.com>
2021-11-15 13:27:32 -0600

    Add footer pages to create footer. Also added text to footer.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = (props) => {
+  return (
+    <div className="row row-space">
+      <div className="col-md-12" style={{ padding: 0 }}>
+        <div className="footer">
+          <h4>©2021 Danielle Allen</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
